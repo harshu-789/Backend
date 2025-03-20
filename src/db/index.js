b540baddf1eec76ca58bb0d32cca1a2fb9939e41
Rishesha Harsh <risheshaharsh@gmail.com>
2025-03-20 13:53:08 +0530

    Backend

--- src/db/index.js
+++ src/db/index.js
@@ -2,17 +2,14 @@ import 'dotenv/config'
 import mongoose from "mongoose";
 
 import { DB_NAME } from "../constant.js";
-// import dotenv from "dotenv"
-// dotenv.config()
+
 
 const connectDB = async () => {
     console.log(process.env.MONGODB_URI);
     
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`, 
+        );
 
         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import 'dotenv/config'
-// import dotenv from "dotenv"
-// dotenv.config({
-//     path: './env'
-// })
+
 import connectDB from "./db/index.js";
 console.log(process.env.MONGODB_URI)
 connectDB()
